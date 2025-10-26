f544915e795f61e980d0e709e765bfb89d44a36e
kenny260 <n.binthia@alustudent.com>
2025-10-26 13:18:46 +0200

    567

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""Script that fetches the first 10 hot posts for a given subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a valid subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {'limit': 10}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    # Check for invalid subreddit
-    if response.status_code != 200:
-        print("None")
-        return
-
-    posts = response.json().get("data", {}).get("children", [])
-    if not posts:
-        print("None")
-        return
-
-    for post in posts:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
+        print(None)
