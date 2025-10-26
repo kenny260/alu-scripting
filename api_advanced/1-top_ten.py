17cbd330f94458bb1bc248893ae1a0ee2ec267f1
kenny260 <n.binthia@alustudent.com>
2025-10-26 13:01:33 +0200

    567

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,19 +12,18 @@ def top_ten(subreddit):
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 10}
 
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
-        data = response.json().get("data", {}).get("children", [])
-        if not data:
-            print(None)
-            return
-
-        for post in data:
-            print(post.get("data", {}).get("title"))
-    except Exception:
+    if response.status_code != 200:
         print(None)
+        return
+
+    posts = response.json().get("data", {}).get("children", [])
+    if not posts:
+        print(None)
+        return
+
+    for post in posts:
+        print(post.get("data", {}).get("title"))
+
 
