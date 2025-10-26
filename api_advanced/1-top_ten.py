b397f1ef05cd63a0d06eaf8083db61c75f3fcfc8
kenny260 <n.binthia@alustudent.com>
2025-10-26 13:49:02 +0200

    567

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,36 +5,20 @@ import requests
 
 def top_ten(subreddit):
     """Main function"""
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
-
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "MyRedditApp/0.0.1"}
+    headers = {"User-Agent": "python:myapp:v1.0"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the request was successful
-        if response.status_code != 200:
-            print(None)
-            return
-
-        json_data = response.json()
         
-        if json_data.get("data") is None:
+        if response.status_code == 200:
+            data = response.json().get("data")
+            children = data.get("children")
+            
+            for post in children:
+                print(post.get('data').get('title'))
+        else:
             print(None)
-            return
-
-        children = json_data.get("data").get("children")
-
-        if children is None:
-            print(None)
-            return
-
-        for post in children:
-            title = post.get('data').get('title')
-            print(title)
-
+            
     except Exception:
         print(None)
